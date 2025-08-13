d2f0699657294a3432752bd6f224ef77937ea1ca
cPaz08 <pazvaldivia0807@gmail.com>
2025-08-12 20:41:47 -0500

    first class

---
+++ 1.os-info.js
@@ -0,0 +1,9 @@
+const os = require('node:os')
+
+console.log('Información del sistema operivo', os.platform())
+console.log('Versión del sistema operivo', os.release())
+console.log('Arquitectura', os.arch())
+console.log('CPUs', os.cpus()) // <-- vamos a poder escalar procesos en Node
+console.log('Memoria libre', os.freemem() / 1024 / 1024)
+console.log('Memoria total', os.totalmem() / 1024 / 1024)
+console.log('uptime', os.uptime()/60/60)
---
+++ 1.os-info.mjs
@@ -0,0 +1,9 @@
+import { platform, release, arch, cpus, freemem, totalmem, uptime } from 'node:os'
+
+console.log('Información del sistema operivo', platform())
+console.log('Versión del sistema operivo', release())
+console.log('Arquitectura', arch())
+console.log('CPUs', cpus()) // <-- vamos a poder escalar procesos en Node
+console.log('Memoria libre', freemem() / 1024 / 1024)
+console.log('Memoria total', totalmem() / 1024 / 1024)
+console.log('uptime', uptime()/60/60)
